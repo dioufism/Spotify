0844a5a19480dafbc39cb002107e22c6563cd232
Ousmane <dioufism@gmail.com>
2021-03-10 20:00:47 -0500

    Setup file organization into different group Deleted  storyboard into Plist File Setup root window inside App delegate setup main scene for scene delegate both the above help set the UI programmatically Configured group hierarchy and added files for the different ViewControllers that be needed in this project

--- Spotify/Ressources/AppDelegate.swift
+++ Spotify/Ressources/AppDelegate.swift
@@ -10,11 +10,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let window  =  UIWindow(frame: UIScreen.main.bounds) // setting the windw
+        window.rootViewController = HomeViewController()
+        window.makeKeyAndVisible()
+        self.window = window
         return true
+        
+       
     }
 
     // MARK: UISceneSession Lifecycle
